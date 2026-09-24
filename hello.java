e2b16a6f5127b060890321cf9f33919ed0ffda2c
223118karthick-byte <223118karthick@gmail.com>
2026-09-25 05:14:37 +0530

    Java Error

--- hello.java
+++ hello.java
@@ -1,10 +1,26 @@
+// class hello{
+//     public static void main (String args[]){
+//        int a= 10;
+//        int b= 10;
+//        int c=a+b;
+//        System.out.print(c);
+//     }
+
+
+// }
 class hello{
     public static void main (String args[]){
-       int a= 10;
-       int b= 10;
-       int c=a+b;
-       System.out.print(c);
+        int a= 29;
+        int cas =99;
+        int bank =2999;
+        char A="A IN A";
+        String name = "hello";
+        boolean isTrue = true; 
+        float f= 10.8999999;    
+
+
+        System.out.println(a,cas,bank,A,name,isTrue,f);
+
+
     }
-
-
 }
