51dbf37b1101014ba9c98f334656f84d8884c61e
223118karthick-byte <223118karthick@gmail.com>
2026-09-24 20:46:09 +0530

    Python learn

--- python.py
+++ python.py
@@ -1,4 +1,9 @@
-for i in range (1,6):
-    for j in range (i):
-        print("*", end="")
-    print()
+# for i in range (1,6):
+#     for j in range (i):
+#         print("*", end="")
+#     print()
+# i=3;
+# for i in range (5):
+#     print ("I am Karthick")
+def student():
+    name
