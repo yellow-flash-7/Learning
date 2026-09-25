2a5a33abd44dfc9ceac0235029dd5fd9ee9477d0
223118karthick-byte <223118karthick@gmail.com>
2026-09-25 20:59:50 +0530

    hi err

--- hello.java
+++ hello.java
@@ -8,19 +8,12 @@
 
 
 // }
+// 
 class hello{
     public static void main (String args[]){
-        int a= 29;
-        int cas =99;
-        int bank =2999;
-        String A ="A IN A";
-        String name = "hello";
-        boolean isTrue = true; 
-        float f= 10.59999909099989898f;    
-
-
-        System.out.println(a+"\n"+cas+"\n"+bank+"\n"+A+"\n"+name+"\n"+isTrue+"\n"+f);
-
-
-    }
+       int a= 10;
+       int b= 10;
+       int c=a+b;
+       System.out.print(c);
+    }")
 }
